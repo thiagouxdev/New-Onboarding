5446e572a210de2bd1540ef0d9294e7acb072c6d
Thiago UX <contato.dev.thiagosilva@gmail.com>
2024-06-10 10:34:22 -0300

    alteração pagina de medias

--- lib/utils/constants/media_strings.dart
+++ lib/utils/constants/media_strings.dart
@@ -4,13 +4,13 @@ class TMedias {
 
   // -- OnBoarding Images
   static const String onBoardingMedia1 =
-      "assets/images/on_onboarding_images/video_01.png";
+      "assets/images/on_onboarding_medias/video_01.png";
   static const String onBoardingMedia2 =
-      "assets/images/on_onboarding_images/img_01.png";
+      "assets/images/on_onboarding_medias/img_01.png";
   static const String onBoardingMedia3 =
-      "assets/images/on_onboarding_images/img_02.png";
+      "assets/images/on_onboarding_medias/img_02.png";
   static const String onBoardingMedia4 =
-      "assets/images/on_onboarding_images/img_03.png";
+      "assets/images/on_onboarding_medias/img_03.png";
   static const String onBoardingMedia5 =
-      "assets/images/on_onboarding_images/img_04.png";
+      "assets/images/on_onboarding_medias/img_04.png";
 }

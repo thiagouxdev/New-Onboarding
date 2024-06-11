53d8c5029579cbe053a0332fa003fe5e4f2255d9
Thiago UX <contato.dev.thiagosilva@gmail.com>
2024-06-10 21:36:00 -0300

    video qldd baixa

--- lib/utils/constants/media_strings.dart
+++ lib/utils/constants/media_strings.dart
@@ -4,7 +4,7 @@ class TMedias {
 
   // -- OnBoarding Images
   static const String onBoardingMedia1 =
-      "assets/images/on_onboarding_medias/new_onboarding_alta.mp4";
+      "assets/images/on_onboarding_medias/new_onboarding_qlddbaixa.mp4";
   static const String onBoardingMedia2 =
       "assets/images/on_onboarding_medias/img_01.png";
   static const String onBoardingMedia3 =
